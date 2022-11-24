5ae62e47d444bc9e393844c5d3fc701d2d15c404
snehaarun-2000 <118956097+snehaarun-2000@users.noreply.github.com>
2022-11-24 10:58:58 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+st.write("""
+# Multiplication of 2 numbers
+""")
+#Get Input
+
+st.header('Input Parameters')
+
+def user_input_features():
+
+    num1 = st.number_input("NUMBER 1",min_value=0.0,max_value=2000000.0)
+    num2 = st.number_input("NUMBER 2",min_value=0.0,max_value=2000000.0)
+    
+
+st.write(num1*num2)
