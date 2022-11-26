2793d7a3049258c103694fcba635e331953136b3
snehaarun-2000 <118956097+snehaarun-2000@users.noreply.github.com>
2022-11-26 11:28:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,14 @@ def user_input_features():
     num1 = st.number_input("NUMBER 1",min_value=0.0,max_value=2000000.0)
     num2 = st.number_input("NUMBER 2",min_value=0.0,max_value=2000000.0)
     
+    data = {'NUMBER 1': num1,
+            'NUMBER 2': num2
+            }
+    features = pd.DataFrame(data, index=[0])
+    return features
+    
 df = user_input_features()
-
-st.write(num1*num2)
+st.write(df.to_dict())
+continous_features = ['NUMBER 1','NUMBER 2']
+L=df['NUMBER 1']*df['NUMBER 2']
+st.write(L)
