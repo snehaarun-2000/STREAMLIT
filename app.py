ff59aa4242801da1d9bd5b1a1f5f95296862353a
snehaarun-2000 <118956097+snehaarun-2000@users.noreply.github.com>
2022-11-26 10:54:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
-import pickle
 
 st.write("""
 # Multiplication of 2 numbers
