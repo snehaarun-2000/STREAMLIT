eaa0c50830ccfa35411ba584a317b0690a1bffe8
snehaarun-2000 <118956097+snehaarun-2000@users.noreply.github.com>
2022-11-26 12:10:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 import pickle
@@ -12,19 +11,12 @@ st.write("""
 
 st.header('Input Parameters')
 
-def user_input_features():
+num1 = st.number_input("NUMBER 1",min_value=0.0,max_value=2000000.0)
+num2 = st.number_input("NUMBER 2",min_value=0.0,max_value=2000000.0)
+    
+data = {'NUMBER 1': num1,'NUMBER 2': num2}
 
-    num1 = st.number_input("NUMBER 1",min_value=0.0,max_value=2000000.0)
-    num2 = st.number_input("NUMBER 2",min_value=0.0,max_value=2000000.0)
-    
-    data = {'NUMBER 1': num1,
-            'NUMBER 2': num2
-            }
-    features = pd.DataFrame(data, index=[0])
-    return features
-    
-df = user_input_features()
-st.write(df.to_dict())
+st.write(data)
 continous_features = ['NUMBER 1','NUMBER 2']
-L=df['NUMBER 1']*df['NUMBER 2']
-st.write(L)
+st.subheader('Product of NUMBER 1 AND NUMBER 2')
+st.write(num1*num2)
